da3ee3595f171b32c782c3941352bf79c0545bce
Paul7772 <pmikhanov2010@mail.ru>
2024-04-25 08:24:21 +0500

    Test work model

--- infocountry/models.py
+++ infocountry/models.py
@@ -4,4 +4,7 @@ from django.db import models
 class Country(models.Model):
     name = models.CharField(max_length=20)
     capital = models.CharField(max_length=15)
+    population = models.IntegerField()
+    square = models.IntegerField()
+    language = models.CharField(max_length=15)
     description = models.TextField()
