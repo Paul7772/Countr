5f2f73b684f4c40c9ce0b015c13fe4facc1a0eea
Paul7772 <pmikhanov2010@mail.ru>
2024-05-02 00:11:21 +0500

    idk

--- infocountry/models.py
+++ infocountry/models.py
@@ -2,9 +2,19 @@ from django.db import models
 
 
 class Country(models.Model):
-    name = models.CharField(max_length=20)
+    name = models.CharField(max_length=20, unique=True)
+    slug = models.SlugField(max_length=255, unique=True, db_index=True)
     capital = models.CharField(max_length=15)
     population = models.IntegerField()
     square = models.IntegerField()
-    language = models.CharField(max_length=15)
+    language = models.CharField(max_length=100)
     description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['-name']
+        indexes = [
+            models.Index(fields=['name'])
+        ]
--- infocountry/views.py
+++ infocountry/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, HttpResponse
 from .models import Country
+
 
 def index(request):
     country = Country.objects.all()
-    return render(request, 'infocountry/index.html', {'country': country})
+    return render(request, 'infocountry/index.html', {'countries': country})
+
+
+def show_descript(request, country_slug):
+    country = get_object_or_404(Country, slug=country_slug)
+    return render(request, 'infocountry/show_descriptions.html', {'countries': [country]})
